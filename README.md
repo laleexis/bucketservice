645790aac33e3ccfb31b29b86301792a92a5b2c2
Alexis Lucero <alexisadrianlucero@gmail.com>
2019-12-30 17:23:42 -0300

    update

--- README.md
+++ README.md
@@ -1,32 +1,31 @@
-# bucketservice
+# Bucketservice
 Downlad/upload files in bucket s3
 
-requirements:
-````
-boto3
-````
-usage:
+###requirements:
 
-Insert accees key and secret key in code
+`boto3`
 
-In "BUCKET" variable, insert the aws bucket name
+#Usage:
 
-Download:
+- Insert accees key and secret key in code
 
-1 Run the script and enter option 2 "download file"
+- In `BUCKET` variable, insert the aws bucket name
+
+###Download:
+
+- Run the script and enter option 2 `download file`
 
 the files hosted in the bucket will be listed
 
-2 Enter the key you want to download
+- Enter the key you want to download
 
 the file will be downloaded in the same location as the script
 
-Upload:
+###Upload:
 
-in LOCAL_FILE enter the file name you want to download (
+in ``LOCAL_FILE`` enter the file name you want to download (
 the file must be in the same location as the script)
 
-1 Run the sctipt and enter option 2 "upload file"
-
+- Run the sctipt and enter option 2 `upload file`
 
 
--- uploads3.py
+++ uploads3.py
@@ -1,11 +1,12 @@
 import boto3
-from botocore.exceptions import NoCredentialsError
+from botocore.exceptions import NoCredentialsError, ClientError
+
 import botocore
 
-ACCESS_KEY = ''
-SECRET_KEY = ''
-BUCKET = ""
-LOCAL_FILE = ""
+ACCESS_KEY = 'AKIAVHP33H4XNVOG3YR2'
+SECRET_KEY = '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'
+BUCKET = "supportmeli"
+LOCAL_FILE = "uploadtest12.txt"
 S3_FILENAME = LOCAL_FILE
 
 print(" Menu: \n1 Upload file\n2 Download file")
@@ -13,13 +14,14 @@ selection = int(input("Select option"))
 
 
 def upload_to_aws(local_file, bucket, s3_file):
-    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
-                      aws_secret_access_key=SECRET_KEY)
-
     try:
+        s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
+                          aws_secret_access_key=SECRET_KEY)
         s3.upload_file(local_file, bucket, s3_file)
         print("Upload Successful")
         return True
+    except ClientError as c:
+        print(c)
     except FileNotFoundError:
         print("The file was not found")
         return False
