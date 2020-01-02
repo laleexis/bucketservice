7435b65e6b80193688990b64b213b59f2281e35b
Lucero <alucero@deloitte.com>
2020-01-02 12:27:58 -0300

    update

--- uploads3.py
+++ uploads3.py
@@ -2,16 +2,23 @@ import boto3
 import json
 from botocore.exceptions import NoCredentialsError, ClientError
 import botocore
+from os import listdir
 
-ACCESS_KEY = ''
-SECRET_KEY = ''
-BUCKET = ""
-LOCAL_FILE = "uploadtest12.txt"
-S3_FILENAME = LOCAL_FILE
+
+with open("config.json") as json_data_file:
+    data = json.load(json_data_file)
+print(data["ACCESS_KEY"])
+
+ACCESS_KEY = data["ACCESS_KEY"]
+SECRET_KEY = data["SECRET_KEY"]
+BUCKET = data["BUCKET"]
+LOCAL_PATH = data["LOCAL_PATH"]
 
 print(" Menu: \n1 Upload file\n2 Download file")
 selection = int(input("Select option"))
 
+def ls(ruta=""):
+    return listdir(ruta)
 
 def upload_to_aws(local_file, bucket, s3_file):
     try:
@@ -43,31 +50,39 @@ def get_all_s3_keys(bucket):
             break
 
     return keys
-
-
-if selection == 1:
-    print("Uploading file")
-    uploaded = upload_to_aws(LOCAL_FILE, BUCKET, S3_FILENAME)
-
-elif selection == 2:
-    print("Download files")
-    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
-                      aws_secret_access_key=SECRET_KEY)
-    s3.list_objects_v2(Bucket=BUCKET)
-    print(get_all_s3_keys(BUCKET))
-    KEY = str(input("enter key"))  # replace with your object key
-
-    s3 = boto3.resource('s3',
-                        aws_access_key_id=ACCESS_KEY,
-                        aws_secret_access_key=SECRET_KEY)
-
+def download_from_aws(bucket,key,key):
     try:
-        s3.Bucket(BUCKET).download_file(KEY, KEY)
+        s3.Bucket(bucket).download_file(key,key)
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == "404":
             print("The object does not exist.")
         else:
             raise
+
+
+
+if selection == 1:
+
+    print(ls(LOCAL_PATH))
+    LOCAL_FILE = input(str("Select file to upload"))
+    S3_FILENAME= LOCAL_FILE
+
+    print("Uploading file")
+    uploaded = upload_to_aws(LOCAL_FILE, BUCKET, S3_FILENAME)
+
+elif selection == 2:
+    print("listing files...")
+    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
+                      aws_secret_access_key=SECRET_KEY)
+    print(s3.list_objects_v2(Bucket=BUCKET))
+    print(get_all_s3_keys(BUCKET))
+    KEY = str(input("enter key"))
+
+    s3 = boto3.resource('s3',
+                        aws_access_key_id=ACCESS_KEY,
+                        aws_secret_access_key=SECRET_KEY)
+
+    download_from_aws(BUCKET,KEY,KEY)
 
 else:
     print("wrong option")
