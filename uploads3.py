0900355268b1feeef936eeff9f5dd7e8d4542bb1
Lucero <alucero@deloitte.com>
2020-01-02 16:44:01 -0300

    args added

--- uploads3.py
+++ uploads3.py
@@ -4,21 +4,18 @@ from botocore.exceptions import NoCredentialsError, ClientError
 import botocore
 from os import listdir
 import pyfiglet
-
-#crear un json con los datos
-with open("config.json") as json_data_file:
-    data = json.load(json_data_file)
-
-ACCESS_KEY = data["ACCESS_KEY"]
-SECRET_KEY = data["SECRET_KEY"]
-BUCKET = data["BUCKET"]
-LOCAL_PATH = data["LOCAL_PATH"]
+import sys
 
 ascii_banner = pyfiglet.figlet_format("DOWN/UP S3")
 print(ascii_banner)
-print(" Menu: \n1 Upload file\n2 Download file")
-selection = int(input("Select option: "))
+with open("config.json") as json_data_file:
+        data = json.load(json_data_file)
+ACCESS_KEY = data["ACCESS_KEY"]
+SECRET_KEY = data["SECRET_KEY"]
+LOCAL_PATH = data["LOCAL_PATH"]
 
+
+#funciones
 def ls(ruta=""):
     return listdir(ruta)
 
@@ -38,6 +35,8 @@ def upload_to_aws(local_file, bucket, s3_file):
 
 
 def get_all_s3_keys(bucket):
+    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
+                          aws_secret_access_key=SECRET_KEY)
     keys = []
 
     kwargs = {'Bucket': bucket}
@@ -53,39 +52,52 @@ def get_all_s3_keys(bucket):
 
     return keys
 def download_from_aws(bucket,key,s3key):
+    s3 = boto3.resource('s3',aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY)
     try:
         s3.Bucket(bucket).download_file(key,key)
+        print("Download Successful")
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == "404":
             print("The object does not exist.")
         else:
             raise
+if len(sys.argv) ==1:
 
+#Modo menu
+#crear un json con los datos   
+    BUCKET = data["BUCKET"]
+    
+    print(" Menu: \n1 Upload file\n2 Download file")
+    selection = int(input("Select option: "))
 
+    if selection == 1:
+        print("Listing files..")
+        print(ls(LOCAL_PATH))
+        s3name= input(str("Select file to upload: "))
+        S3_FILENAME= s3name
+        LOCAL_FILE = str(LOCAL_PATH)+ s3name
+        S3_FILENAME= s3name
+        print(LOCAL_FILE)
+        print("Uploading file...")
+        uploaded = upload_to_aws(LOCAL_FILE, BUCKET, S3_FILENAME)
 
-if selection == 1:
-    print("Listing files..")
-    print(ls(LOCAL_PATH))
-    s3name= input(str("Select file to upload: "))
-    S3_FILENAME= s3name
-    LOCAL_FILE = str(LOCAL_PATH)+ s3name
-    S3_FILENAME= s3name
-    print(LOCAL_FILE)
-    print("Uploading file...")
-    uploaded = upload_to_aws(LOCAL_FILE, BUCKET, S3_FILENAME)
+    elif selection == 2:
+        print("listing files...")
+        s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
+                          aws_secret_access_key=SECRET_KEY)
+        print(get_all_s3_keys(BUCKET))
+        KEY = str(input("enter key: "))
+        download_from_aws(BUCKET,KEY,KEY)
 
-elif selection == 2:
-    print("listing files...")
-    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
-                      aws_secret_access_key=SECRET_KEY)
-    print(get_all_s3_keys(BUCKET))
-    KEY = str(input("enter key: "))
-
-    s3 = boto3.resource('s3',
-                        aws_access_key_id=ACCESS_KEY,
-                        aws_secret_access_key=SECRET_KEY)
-
-    download_from_aws(BUCKET,KEY,KEY)
-
+    else:
+        print("wrong option")
 else:
-    print("wrong option")
+    if sys.argv[1] == "-u":
+        print("Uploading Files")
+        S3_FILENAME=sys.argv[2]
+        uploaded = upload_to_aws(sys.argv[3], sys.argv[2], sys.argv[4])
+    elif sys.argv[1]=="-d":
+        download_from_aws(sys.argv[3],sys.argv[2],sys.argv[4])
+    else:
+        print("select [-d] to donwload, [-u] to upload or none for menu")
+
