5d72abf691edc23e27d4968e62947ce17e4a6cd2
Lucero <alucero@deloitte.com>
2020-01-07 15:28:17 -0300

    code update

--- updowns3.py
+++ updowns3.py
@@ -13,6 +13,9 @@ ACCESS_KEY = data["ACCESS_KEY"]
 SECRET_KEY = data["SECRET_KEY"]
 LOCAL_PATH = data["LOCAL_PATH"]
 
+s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
+                          aws_secret_access_key=SECRET_KEY)
+s3r = boto3.resource('s3',aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY)
 
 #funciones
 def ls(route=""):
@@ -25,8 +28,6 @@ def ls(route=""):
 
 def upload_to_aws(local_file, bucket,s3filename):
     try:
-        s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
-                          aws_secret_access_key=SECRET_KEY)
         s3.upload_file(local_file, bucket, s3filename)
         print("Upload Successful")
         return True
@@ -38,8 +39,6 @@ def upload_to_aws(local_file, bucket,s3filename):
         return False
 
 def get_all_s3_buckets():
-    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
-                          aws_secret_access_key=SECRET_KEY)
     response = s3.list_buckets()
     bucketdict = {}
     cont = 1
@@ -50,8 +49,6 @@ def get_all_s3_buckets():
     print("Listing buckets...")
     return bucketdict
 def get_all_s3_keys(bucket):
-    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
-                          aws_secret_access_key=SECRET_KEY)
     keys = []
     keysdict = {}
     cont = 1
@@ -59,17 +56,15 @@ def get_all_s3_keys(bucket):
         keys.append("["+str(cont)+"] "+key['Key'])
         keysdict[cont]= key['Key']
         cont=cont+1
-    #print(keysdict)
     return keysdict
 def delete_from_aws(bucket,key):
-    s3 = boto3.resource('s3',aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY)
-    s3.Object(bucket,key).delete()
+    s3r.Object(bucket,key).delete()
+    print("delete Successful")
     return ("deleted")
 
 def download_from_aws(bucket,key,s3key):
-    s3 = boto3.resource('s3',aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY)
     try:
-        s3.Bucket(bucket).download_file(key,key)
+        s3r.Bucket(bucket).download_file(key,key)
         print("Download Successful")
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == "404":
@@ -79,8 +74,8 @@ def download_from_aws(bucket,key,s3key):
 if len(sys.argv) ==1:
     print("modo API")
 
-
-else:#Modo por parametros
+#Modo por parametros
+else:
     if sys.argv[1] == "-u":
         S3_FILENAME=sys.argv[2]
         print("Uploading Files")
@@ -91,18 +86,20 @@ else:#Modo por parametros
     elif sys.argv[1]=="-d":
         if len(sys.argv)==3:
             download_from_aws(data["BUCKET"],sys.argv[2],sys.argv[2])
+
         else:
             download_from_aws(sys.argv[3],sys.argv[2],sys.argv[2])
+
     elif sys.argv[1]=="-h":
-        print(" To download:  [-d] [file] [bucket]\n To upload: [-u] [file] [bucket]\n To show menu: no args")
+        print(" To download:  [-d] [file] [bucket]\n To upload: [-u] [file] [bucket]\n To show menu: [-m]")
+
     elif sys.argv[1]=="-m":
         ascii_banner = pyfiglet.figlet_format("DOWN&UPS3")
         print(ascii_banner)
-        #Modo menu
+#Modo menu
 #crear un json con los datos   
         BUCKET = data["BUCKET"]
-    
-        print(" Menu: \n1 Upload file\n2 Download file")
+        print(" Menu: \n1 Upload file\n2 Download file\n3 Delete file")
         selection = int(input("Select option: "))
 
         if selection == 1:
@@ -113,7 +110,6 @@ else:#Modo por parametros
             S3_FILENAME= listf[s3name]
             LOCAL_FILE = str(LOCAL_PATH)+ listf[s3name]
             S3_FILENAME= listf[s3name]
-            print(LOCAL_FILE)
             bucketdown = get_all_s3_buckets()
             print(bucketdown)
             BUCKETSEL= int(input("Select Bucket: "))
@@ -121,8 +117,6 @@ else:#Modo por parametros
             uploaded = upload_to_aws(LOCAL_FILE, bucketdown[BUCKETSEL], S3_FILENAME)
 
         elif selection == 2:
-            s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
-                            aws_secret_access_key=SECRET_KEY)
             bucketdown = get_all_s3_buckets()
             print(bucketdown)
             BUCKETSEL= int(input("Select Bucket: "))
@@ -131,6 +125,16 @@ else:#Modo por parametros
             KEY = int(input("enter key: "))
             download_from_aws(bucketdown[BUCKETSEL],keysdown[KEY],keysdown[KEY])
 
+        elif selection == 3:
+            bucketdown = get_all_s3_buckets()
+            print(bucketdown)
+            BUCKETSEL= int(input("Select Bucket: "))
+            keysdown = get_all_s3_keys(bucketdown[BUCKETSEL])
+            print(keysdown)
+            KEY = int(input("enter key: "))
+            delete_from_aws(bucketdown[BUCKETSEL],keysdown[KEY],)
+
         else:
             print("wrong option")
-
+    else:
+        print(" To download:  [-d] [file] [bucket]\n To upload: [-u] [file] [bucket]\n To show menu: [-m]")        
