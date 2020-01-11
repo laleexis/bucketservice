3e58add385d0c79f2ff659dbf56640c34511d880
Alexis Lucero <alexisadrianlucero@gmail.com>
2020-01-11 16:34:16 -0300

    reformat code

--- api.py
+++ api.py
@@ -9,34 +9,40 @@ path = "C:/Users/alucero/Documents"
 
 @app.route('/')
 def entry_point():
-	return 'Test API'
+    return 'Test API'
 
-@app.route("/files", methods=['POST']) #json in body
+
+@app.route("/files", methods=['POST'])  # json in body
 def storage():
-	req_data = request.get_json()
-	if request.method == "POST":
-		contents = get_all_s3_keys(req_data['BUCKET'],req_data['ACCESS_KEY'],req_data['SECRET_KEY'])
-		return contents
+    req_data = request.get_json()
+    if request.method == "POST":
+        contents = get_all_s3_keys(req_data['BUCKET'], req_data['ACCESS_KEY'], req_data['SECRET_KEY'])
+        return contents
+
 
 @app.route("/files/upload", methods=['POST'])
 def upload():
-	req_data = request.get_json()
-	if request.method == "POST":
-		output= upload_to_aws(req_data['UPLOAD'], req_data['BUCKET'],req_data['NAME'],req_data['ACCESS_KEY'],req_data['SECRET_KEY'])
-		return "Upload complete"
+    req_data = request.get_json()
+    if request.method == "POST":
+        output = upload_to_aws(req_data['UPLOAD'], req_data['BUCKET'], req_data['NAME'], req_data['ACCESS_KEY'],
+                               req_data['SECRET_KEY'])
+        return "Upload complete"
+
 
 @app.route("/files/<filename>", methods=['GET'])
 def download(filename):
-	if request.method == 'GET':
-		output = download_from_aws(filename,BUCKET,path)
-		 
-		return send_file(output, as_attachment=True)
+    if request.method == 'GET':
+        output = download_from_aws(filename, BUCKET, path)
+
+        return send_file(output, as_attachment=True)
+
 
 @app.route("/files/<filename>", methods=['DELETE'])
 def delete(filename):
-	if request.method == 'DELETE':
-		output = delete_from_aws(BUCKET,filename)
-		return "Deleted"
-		
+    if request.method == 'DELETE':
+        output = delete_from_aws(BUCKET, filename)
+        return "Deleted"
+
+
 if __name__ == '__main__':
-	app.run('0.0.0.0',debug=True,port=80)
+    app.run('0.0.0.0', debug=True, port=80)
--- updowns3.py
+++ updowns3.py
@@ -1,58 +1,68 @@
 import boto3
 from os import listdir
 
-#funciones
-def client(ACCESS_KEY,SECRET_KEY):
+
+# funciones
+def client(ACCESS_KEY, SECRET_KEY):
     s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
-                          aws_secret_access_key=SECRET_KEY)
+                      aws_secret_access_key=SECRET_KEY)
     return s3
 
-def resource(ACCESS_KEY,SECRET_KEY):
-    s3r = boto3.resource('s3',aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY)
+
+def resource(ACCESS_KEY, SECRET_KEY):
+    s3r = boto3.resource('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
     return s3r
+
 
 def ls(route=""):
     files = {}
     cont = 1
     for f in listdir(route):
         files[cont] = f
-        cont +=1
+        cont += 1
     return files
 
-def upload_to_aws(local_file, bucket,s3filename,ACCESS_KEY,SECRET_KEY):
-    s3 = client(ACCESS_KEY,SECRET_KEY)
+
+def upload_to_aws(local_file, bucket, s3filename, ACCESS_KEY, SECRET_KEY):
+    s3 = client(ACCESS_KEY, SECRET_KEY)
     s3.upload_file(local_file, bucket, s3filename)
     print("Upload Successful")
 
-def get_all_s3_buckets(ACCESS_KEY,SECRET_KEY):
-    s3 = client(ACCESS_KEY,SECRET_KEY)
+
+def get_all_s3_buckets(ACCESS_KEY, SECRET_KEY):
+    s3 = client(ACCESS_KEY, SECRET_KEY)
     response = s3.list_buckets()
     bucketdict = {}
     cont = 1
     for bucket in response['Buckets']:
         bucketdict[cont] = bucket['Name']
-        cont+=1
-   
+        cont += 1
+
     print("Listing buckets...")
     return bucketdict
-def get_all_s3_keys(bucket,ACCESS_KEY,SECRET_KEY):
-    s3 = client(ACCESS_KEY,SECRET_KEY)
+
+
+def get_all_s3_keys(bucket, ACCESS_KEY, SECRET_KEY):
+    s3 = client(ACCESS_KEY, SECRET_KEY)
     keys = []
     keysdict = {}
     cont = 1
     for key in s3.list_objects(Bucket=bucket)['Contents']:
-        keys.append("["+str(cont)+"] "+key['Key'])
-        keysdict[cont]= key['Key']
-        cont=cont+1
+        keys.append("[" + str(cont) + "] " + key['Key'])
+        keysdict[cont] = key['Key']
+        cont = cont + 1
     return keysdict
-def delete_from_aws(bucket,key,ACCESS_KEY,SECRET_KEY):
-    s3r= resource(ACCESS_KEY,SECRET_KEY)
-    s3r.Object(bucket,key).delete()
+
+
+def delete_from_aws(bucket, key, ACCESS_KEY, SECRET_KEY):
+    s3r = resource(ACCESS_KEY, SECRET_KEY)
+    s3r.Object(bucket, key).delete()
     print("delete Successful")
     return ("deleted")
 
-def download_from_aws(key,bucket,path,ACCESS_KEY,SECRET_KEY):
-    s3r= resource(ACCESS_KEY,SECRET_KEY)
+
+def download_from_aws(key, bucket, path, ACCESS_KEY, SECRET_KEY):
+    s3r = resource(ACCESS_KEY, SECRET_KEY)
     output = f"{path}{key}"
     s3r.Bucket(bucket).download_file(key, output)
     return output
